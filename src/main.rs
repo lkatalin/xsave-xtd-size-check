316ef71f57eb95b7ec35c812bbfd1f7e0dbc74df
lkatalin <lkatalin@users.noreply.github.com>
2020-01-30 16:35:31 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,13 @@ fn main() {
     
     // set the bits for relevant state components (2, 5, 6, 7, 9)
     // == 0b1011100100
-    // just kidding, don't do this because it will segfault per pg. 313 of 
+    // just kidding, don't do this because it will probably segfault per pg. 313 of 
     // https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-vol-1-manual.pdf
     // unsafe { core::arch::x86_64::_xsetbv(0, 740) };
+    
+    // check if xCRO[7:5] can be set by software
+    let res = unsafe { core::arch::x86_64::__cpuid_count(0x0d, 0x0) };
+    println!("eax bits allowing xcr0[7:5] to be set or not: {:b}", res.eax);
     
     for comp in state_comps.iter() {
         // find state component size and offset
